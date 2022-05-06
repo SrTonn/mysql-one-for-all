2f3b5e65ee6f4339ee00d5492d94dc0a4096623d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-06 19:00:09 -0300

    requisito 11 refatorado

--- desafio11.sql
+++ desafio11.sql
@@ -6,9 +6,10 @@ SELECT
     WHEN song LIKE '%Inner Fire%' THEN REPLACE(song, 'Inner Fire', 'Project')
     WHEN song LIKE '%Silly%' THEN REPLACE(song, 'Silly', 'Nice')
     WHEN song LIKE '%Circus%' THEN REPLACE(song, 'Circus', 'Pull Request')
-    ELSE ''
+    ELSE NULL
   END AS novo_nome
 FROM SpotifyClone.songs
-WHERE song
-  REGEXP 'Streets|Her Own|Inner Fire|Silly|Circus'
+HAVING novo_nome IS NOT NULL
+-- WHERE song
+--   REGEXP 'Streets|Her Own|Inner Fire|Silly|Circus'
 ORDER BY song;
